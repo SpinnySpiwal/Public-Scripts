d2e336c962a2c0ca83bcb84c253830c8136aa6cd
SpinnySpiwal <99829343+SpinnySpiwal@users.noreply.github.com>
2024-08-14 19:13:44 +0100

    Update loader.lua

--- WEAO_Roblox/loader.lua
+++ WEAO_Roblox/loader.lua
@@ -1,1 +1,3 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/SpinnySpiwal/Public-Scripts/main/WEAO_Roblox/weao.lua"))
+--> WEAO For Roblox <--
+--> Updated for WhatExpsAreOnline by SpinnySpiwal :3
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SpinnySpiwal/Public-Scripts/main/WEAO_Roblox/weao.lua"))()
